4c2dc53551513cf8ee28f1a4afe3eb2caf9bc514
intellectdigits <115778808+intellectdigits@users.noreply.github.com>
2023-11-21 23:57:19 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -4,10 +4,26 @@ const sqlite3 = require('sqlite3').verbose();
 const crypto=require("crypto");
 // express app
 const app = express();
+
+const cookieParser = require("cookie-parser");
+const sessions = require('express-session');
 var bodyParser = require('body-parser')
+var fs = require('fs');
+
+var pdf = require('html-pdf');
+
 // create application/x-www-form-urlencoded parser
 var urlencodedParser = bodyParser.urlencoded({ extended: false })
+const oneDay = 1000 * 60 * 60 * 24;
 
+//session middleware
+app.use(sessions({
+secret: "thisismysecrctekey",
+saveUninitialized:true,
+cookie: { maxAge: oneDay },
+resave: false
+}));
+app.use(cookieParser());
 // POST /login gets urlencoded bodies
 app.post('/SetupData', urlencodedParser, function (req, res) {
    const sqlite3 = require('sqlite3').verbose();
@@ -179,6 +195,7 @@ db.close((err) => {
  console.log('Close the database connection.');
 });
 });
+
 app.post('/logdata', urlencodedParser, function (req, res) {
 
  // open database in memory
@@ -202,9 +219,15 @@ db.get(sql, [username,password], (err, row) => {
   if (err) {
     return console.error(err.message);
   }
-  return row
-    ? res.render("index",{user:req.body.username,school:row.school,level:row.email,dept:row.dept})
-    : res.render("login",{error:"Invalid username or password"});
+  if( row){
+  req.session.user = req.body.username ;
+
+  res.render("cat",{user:req.session.user,school:row.school,level:row.email,dept:row.dept})
+}
+    else{
+      res.render("login",{error:"Invalid username or passwordz"});
+    }
+    
 
 });
    
@@ -216,6 +239,46 @@ db.close((err) => {
  console.log('Close the database connection.');
 });
 });
+app.post('/namehint', urlencodedParser, function (req, res) {
+
+  // open database in memory
+ let db = new sqlite3.Database('./db/chinook.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+ });
+ 
+ // close the database connection
+ 
+ db.serialize(() => {
+  // Queries scheduled here will be serialized.
+  let sql = `SELECT *   FROM students
+            WHERE stud_name  LIKE '%${req.body.stud_name}%'`;
+ 
+ // first row only
+ db.get(sql, [], (err, row) => {
+   if (err) {
+     return console.error(err.message);
+   }
+   if( row){
+  res.send(row.stud_name)
+ }
+     else{
+       res.send("no record");
+     }
+     
+ 
+ });
+    
+ });
+ db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+ });
+ });
 // listen for requests
 app.listen(3000);
 
@@ -252,7 +315,7 @@ app.get('/', (req, res) => {
     {title: 'Mario finds stars', snippet: 'Lorem ipsum dolor sit amet consectetur'},
     {title: 'How to defeat bowser', snippet: 'Lorem ipsum dolor sit amet consectetur'},
   ];
-  res.render('index', { title: 'Home', blogs });
+  res.render('login', { user:req.session.username,title: 'Home', blogs });
 });
 
 app.get('/about', (req, res) => {
@@ -264,9 +327,8 @@ app.get('/projects', (req, res) => {
 app.get('/exams', (req, res) => {
   res.render('exams', { title: 'About' });
 });
-app.get('/cat/:id', (req, res) => {
-  let params=(req.params.id);
- let curuser= params.substring(1);
+app.get('/cat', (req, res) => {
+ 
   let db = new sqlite3.Database('./db/chinook.db', (err) => {
     if (err) {
       return console.error(err.message);
@@ -274,11 +336,45 @@ app.get('/cat/:id', (req, res) => {
     console.log('Connected to the in-memory SQlite database.');
    });
    const sql = "SELECT * FROM cat WHERE user=? "
-   db.all(sql, [curuser], (err, rows) => {
+   db.all(sql, [req.session.user], (err, rows) => {
      if (err) {
        return console.error(err.message);
      }
-     res.render("cat", { model: rows });
+     res.render("cat", {user:req.session.user,model: rows });
+   });
+ 
+});
+app.post('/getcat',urlencodedParser, function (req, res) {
+ 
+  let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+   });
+   const sql = "SELECT * FROM cat WHERE user=? and level=? "
+   db.all(sql, [req.session.user,req.body.grade], (err, rows) => {
+     if (err) {
+       return console.error(err.message);
+     }
+     res.send(rows);
+   });
+ 
+});
+app.get('/getclass', (req, res) => {
+ 
+  let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+   });
+   const sql = "SELECT * FROM class"
+   db.all(sql, [], (err, rows) => {
+     if (err) {
+       return console.error(err.message);
+     }
+     res.send(rows);
    });
  
 });
@@ -314,26 +410,220 @@ app.get('/student', (req, res) => {
    });
  
 });
-app.get('/users/:id', (req, res) => {
-  let params=(req.params.id);
-  let curuser= params.substring(1);
+app.get('/users', (req, res) => {
+  
   let db = new sqlite3.Database('./db/chinook.db', (err) => {
     if (err) {
       return console.error(err.message);
     }
     console.log('Connected to the in-memory SQlite database.');
    });
-   const sql = "SELECT * FROM users where school=? "
-   db.all(sql, [curuser], (err, rows) => {
+   const sql = "SELECT * FROM users  "
+   db.all(sql, [], (err, rows) => {
      if (err) {
        return console.error(err.message);
      }
-     res.render("users", { model: rows });
+     res.render("users", {user:req.session.user, model: rows });
+   });
+ 
+});
+app.post('/getreport',urlencodedParser, function (req, res){
+  const pos=1;
+
+  const user=req.body.search;
+  let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+   });
+   const sql = "SELECT * from cat where reg_no=?"
+   db.all(sql, [user], (err, rows) => {
+     if (err) {
+       return console.error(err.message);
+     }
+     if(rows.length>0){
+     let html =`<center><br><br><br><br><br><br><h1>Report Card</h1><img src="images/logo.jpg/>"<br><br><table style="font-size:20pt; text-align:center;"><tr><td>Student Name:${user} </td><td>      </td><td>Class: ${rows[1].level}</td></tr>
+     <tr><td>Position:_______________ </td><td>      </td><td>Out of:____________</td></tr> </table><table style="font-size:15pt" border=1><tr style="background-color:black;color:white;"><th>SUBJECT</th>
+     </th><th>Continous Assesment</th><th>Examination</th></th></th><th>TOTAL</th></th><th>AVERAGE</th></th><th>
+     GRADE</th></tr>`
+     let e=0;
+     let c=0;
+    let t=0;
+    let n=0;
+    rows.forEach(element => {
+      // './test/businesscard.html'
+      t=t+parseInt(element.cat)+parseInt(element.exam);
+      e=e+parseInt(element.exam);
+      c=c+parseInt(element.cat)
+      n=n+1;
+html=html+ `<tr><td>${element.course_code}</td><td>${element.cat}</td><td>${element.exam}</td><td>${parseInt(element.cat)+parseInt(element.exam)}</td>
+<td>${parseInt(element.cat)+parseInt(element.exam)}</td><td>${element.grade}</td>></tr>`
+    
+     
+    })
+    html=html+`<tr><td><b>Total </td> <td>${c}</td><td>${e}</td><td>${t}</td><td>${t}</td><br> </tr>
+   </table></center> <h3><br> Average:${t/n}`
+    var options = { format: 'letter',landscape:true };
+    pdf.create(html, options).toStream(function (err, stream) {
+      if (err) {
+          console.log(err);
+          res.status(500).send("Some kind of error...");
+          return;
+      }
+      stream.pipe(res)
+  })}else{
+    res.send("<center><h1>No Record found for the student")
+  }
+   });
+ 
+  
+
+});
+app.get('/printpreview/:userid', (req, res) => {
+  const pos=1;
+
+  const user=req.params.userid
+  let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+   });
+   const sql = "SELECT * from cat where reg_no=?"
+   db.all(sql, [user.substring(1)], (err, rows) => {
+     if (err) {
+       return console.error(err.message);
+     }
+     let html =`<center><br><br><br><br><br><br><h1>Report Card</h1><img src="images/logo.jpg/>"<br><br><table style="font-size:20pt; text-align:center;"><tr><td>Student Name:${user.substring(1)} </td><td>      </td><td>Class: ${rows[1].level}</td></tr>
+     <tr><td>Position:_______________ </td><td>      </td><td>Out of:____________</td></tr> </table><table style="font-size:15pt" border=1><tr style="background-color:black;color:white;"><th>SUBJECT</th>
+     </th><th>Continous Assesment</th><th>Examination</th></th></th><th>TOTAL</th></th><th>AVERAGE</th></th><th>
+     GRADE</th></tr>`
+     let e=0;
+     let c=0;
+    let t=0;
+    let n=0;
+    rows.forEach(element => {
+      // './test/businesscard.html'
+      t=t+parseInt(element.cat)+parseInt(element.exam);
+      e=e+parseInt(element.exam);
+      c=c+parseInt(element.cat)
+      n=n+1;
+html=html+ `<tr><td>${element.course_code}</td><td>${element.cat}</td><td>${element.exam}</td><td>${parseInt(element.cat)+parseInt(element.exam)}</td>
+<td>${parseInt(element.cat)+parseInt(element.exam)}</td><td>${element.grade}</td>></tr>`
+    
+     
+    })
+    html=html+`<tr><td><b>Total </td> <td>${c}</td><td>${e}</td><td>${t}</td><td>${t}</td><br> </tr>
+   </table></center> <h3><br> Average:${t/n}`
+    var options = { format: 'letter',landscape:true };
+    pdf.create(html, options).toStream(function (err, stream) {
+      if (err) {
+          console.log(err);
+          res.status(500).send("Some kind of error...");
+          return;
+      }
+      stream.pipe(res)
+  })
+   });
+ 
+  
+
+});
+app.get('/scoresheet', (req, res) => {
+ 
+  let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+   });
+   const sql = "SELECT * from courses order by course_title  "
+   db.all(sql, [], (err, rows) => {
+     if (err) {
+       return console.error(err.message);
+     }
+     //res.setHeader('Content-Type', 'application/pdf');
+     res.send(rows)
+  
+   });
+ 
+});
+app.get('/class', (req, res) => {
+ 
+  let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+   });
+   const sql = "SELECT * from class "
+   db.all(sql, [], (err, rows) => {
+     if (err) {
+       return console.error(err.message);
+     }
+     res.render("class", {model: rows });
+  
+   });
+ 
+});
+app.get('/scorenames', (req, res) => {
+ 
+  let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+   });
+   const sql = "SELECT * from students"
+   db.all(sql, [], (err, rows) => {
+     if (err) {
+       return console.error(err.message);
+     }
+     //res.setHeader('Content-Type', 'application/pdf');
+     res.send(rows)
+  
+   });
+ 
+});
+app.post('/name', urlencodedParser, function (req, res){
+ 
+  let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+   });
+   const sql = "SELECT * from cat where reg_no=? order by course_code"
+   db.all(sql, [req.body.stud], (err, rows) => {
+     if (err) {
+       return console.error(err.message);
+     }
+     //res.setHeader('Content-Type', 'application/pdf');
+     res.send(rows)
+     console.log(req.body.stud)
+  
    });
  
 });
 app.get('/result', (req, res) => {
-  res.render('result', { title: 'About' });
+ 
+  let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite database.');
+   });
+   const sql = "SELECT * from students"
+   db.all(sql, [], (err, rows) => {
+     if (err) {
+       return console.error(err.message);
+     }
+     //res.setHeader('Content-Type', 'application/pdf');
+ 
+     res.render("result", {model: rows });
+   });
+ 
 });
 app.post('/addcat', urlencodedParser, function (req, res) {
  
@@ -348,22 +638,33 @@ app.post('/addcat', urlencodedParser, function (req, res) {
    
    db.serialize(() => {
     // Queries scheduled here will be serialized.
-    let sql = `insert into cat(id,semester,course_code,level,user,dept,cat,exam,school,reg_no)values(?,?,?,?,?,?,?,?,?,?)`;
+    let sql = `insert into cat(id,semester,course_code,level,dept,cat,user,exam,school,reg_no,grade)values(?,?,?,?,?,?,?,?,?,?,?)`;
    let semester = req.body.semester;
    let level = req.body.level;
    let course_code = req.body.course_code;
    let exam= req.body.exam;
    let cat = req.body.cat;
    let dept= req.body.dept;
-   let user = req.body.user;
+   let grade="F"
+   let total=parseInt(req.body.exam)+parseInt(req.body.cat)
+   if(total>70){
+     grade="A";
+   }else if(total>60){
+    grade="B";
+   }else if(total>50){
+     grade="C"
+   }else if(total>40){
+grade="D"
+   }
    let school = req.body.school;
    let reg_no = req.body.reg_no;
+
    // first row only
-   db.run(sql, [crypto.randomUUID(),semester,course_code,level,user,dept,cat,exam,school,reg_no], (err, row) => {
+   db.run(sql, [crypto.randomUUID(),semester,course_code,level,dept,cat,course_code,exam,school,reg_no,grade], (err, row) => {
      if (err) {
        return console.error(err.message);
      }else{
-      res.render('cat');
+      res.send('cat');
      }
    console.log(reg_no);
    });
@@ -581,6 +882,79 @@ app.post('/addcourse', urlencodedParser, function (req, res) {
     }
     console.log('Close the database connection.');
    });
+});
+app.post('/addstudent', urlencodedParser, function (req, res) {
+ 
+  let db = new sqlite3.Database('./db/chinook.db', (err) => {
+    if (err) {
+      return console.error(err.message);
+    }
+    console.log('Connected to the in-memory SQlite databases');
+   });
+   
+   // close the database connection
+   
+   db.serialize(() => {
+    // Queries scheduled here will be serialized.
+    let sql = `insert into students(id,stud_name,level,dept,school)values(?,?,?,?,?)`;
+   let stud_name = req.body.stud_name;
+  
+   let level = req.body.level;
+   let dept = req.body.dept;
+   let school = req.body.school;
+
+   // first row only
+   db.run(sql, [crypto.randomUUID(),stud_name,level,dept,school], (err, row) => {
+     if (err) {
+       return console.error(err.message);
+     }else{
+      res.render('students');
+     }
+   
+   });
+      
+   });
+
+ db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+ });
+});
+app.post('/addclass', urlencodedParser, function (req, res) {
+
+let db = new sqlite3.Database('./db/chinook.db', (err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Connected to the in-memory SQlite database.');
+ });
+ 
+ // close the database connection
+ 
+ db.serialize(() => {
+  // Queries scheduled here will be serialized.
+  let sql = `insert into class(id,class,class_teacher)values(?,?,?)`;
+ let level= req.body.level;
+ let class_teacher = req.body.class_teacher;
+
+ db.run(sql, [crypto.randomUUID(),level,class_teacher], (err, row) => {
+   if (err) {
+     return console.error(err.message);
+   }else{
+    res.send('welll');
+   }
+ 
+ });
+    
+ });
+ db.close((err) => {
+  if (err) {
+    return console.error(err.message);
+  }
+  console.log('Close the database connection.');
+ });
 });
 app.get('/setup', (req, res) => {
   res.render('create', { title: 'Create a new blog' });
